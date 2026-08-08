52b8658b162c95e7dd6004c8943686523cbb60d7
Chahat <chahatnandrajog@gmail.com>
2026-08-08 19:57:45 -0400

    Game loop can be reset after game over

--- PacMAn/src/PacMan.java
+++ PacMAn/src/PacMan.java
@@ -251,6 +251,10 @@ public class PacMan extends JPanel implements ActionListener, KeyListener {
         for (Block ghost: ghosts) {
             if (collision(ghost, pacman)) {
                 lives -= 1;
+                if (lives == 0) {
+                    gameOver = true;
+                    return;
+                }
                 resetPositions();
             }
             if (ghost.y == tileSize * 9 && ghost.direction != 'U' && ghost.direction != 'D'){
@@ -302,6 +306,9 @@ public class PacMan extends JPanel implements ActionListener, KeyListener {
     public void actionPerformed(ActionEvent e) {
         move();
         repaint();
+        if (gameOver){
+            gameLoop.stop();
+        }
     }
 
     @Override
@@ -313,6 +320,14 @@ public class PacMan extends JPanel implements ActionListener, KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        if (gameOver) {
+            loadMap();
+            resetPositions();
+            lives = 3;
+            score = 0;
+            gameOver = false;
+            gameLoop.start();
+        }
         //System.out.println("KeyEvent: " + e.getKeyCode()); 
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             pacman.updateDirection('U');
