ae832b10c8c4067753f25d3057c1759190b0d5b4
Chahat <chahatnandrajog@gmail.com>
2026-08-08 02:21:08 -0400

    created representation of objects woo

--- PacMAn/src/PacMan.java
+++ PacMAn/src/PacMan.java
@@ -6,6 +6,28 @@ import javax.swing.*;
 
 public class PacMan extends JPanel {
 
+    class Block {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image image;
+
+        int startX;
+        int startY;
+
+        Block(Image image, int x, int y, int width, int height) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startX = x;
+            this.startY = y;
+        }
+
+    }
+
     private int rowCount = 21; 
     private int columnCount = 19; 
     private int tileSize = 32; 
@@ -23,6 +45,12 @@ public class PacMan extends JPanel {
     private Image pacmanDownImage;
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
+
+    //creating hashset for storing the blocks
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghosts;
+    HashSet<Block> pacman; //oh lawd he comin
 
     PacMan() { //constructor
         setPreferredSize(new Dimension(boardWidth, boardHeight));
